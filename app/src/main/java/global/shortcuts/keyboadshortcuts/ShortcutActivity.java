740c60aa70c1f3dd6e2c6b3cd0da036b17e9e8c9
jonesbr <brandan.jones@uc.edu>
2018-10-08 22:19:58 -0400

    Refactor Activity to superclass.

--- app/src/main/java/global/shortcuts/keyboadshortcuts/ShortcutActivity.java
+++ app/src/main/java/global/shortcuts/keyboadshortcuts/ShortcutActivity.java
@@ -8,13 +8,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 
-public class ShortcutActivity extends AppCompatActivity {
+public class ShortcutActivity extends ShortcutBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shortcut);
+        AutoCompleteTextView actKeys = findViewById(R.id.actKeys);
+        EditText edtShortcutName = findViewById(R.id.edtShortcutName);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -28,25 +33,4 @@ public class ShortcutActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_shortcut, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
