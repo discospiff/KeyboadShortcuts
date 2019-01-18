c82ee89f67fbf7f7aad02a59ae205a6da54b76a2
jonesbr <brandan.jones@uc.edu>
2019-01-17 19:58:33 -0500

    Add button to capture image from gallery.

--- app/src/main/java/global/shortcuts/keyboadshortcuts/ShortcutActivity.java
+++ app/src/main/java/global/shortcuts/keyboadshortcuts/ShortcutActivity.java
@@ -1,9 +1,12 @@
 package global.shortcuts.keyboadshortcuts;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -92,6 +95,18 @@ public class ShortcutActivity extends ShortcutBaseActivity {
         lblAllKeys.setText(currentKeys + key + " " );
         actKeys.setText("");
 
+    }
+
+    @OnClick(R.id.btnOpenGallery)
+    public void onBtnOpenGalleryClicked() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            openGallery();
+        }
+        // TODO handle permission not requested
+    }
+
+    private void openGallery() {
+        // TODO add open gallery logic here.
     }
 
     /**
